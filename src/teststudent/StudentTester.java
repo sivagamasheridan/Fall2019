78924912db3c8dc2a019d7813f6eb760f4b8589a
sivagama <sivagama.srinivasan@sheridanc.on.ca>
2019-09-17 09:30:49 -0400

    TestStudent fall 2019

---
+++ src/teststudent/StudentTester.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teststudent;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+public class StudentTester 
+{
+   public static void main (String args[])
+   {
+    TestStudent[] name= new TestStudent[2];
+   TestStudent s1= new TestStudent();
+   TestStudent s2 = new TestStudent();
+   s1.setName("dfg");
+   s1.setAge(12);
+   s2.setName("ghj");
+   s2.setAge(67);
+           
+   name[0]=s1;
+   name[1]=s2;
+   for(TestStudent i: name)
+   {
+       System.out.println(i.getName()+ " " +i.getAge());
+   }
+}
+}
